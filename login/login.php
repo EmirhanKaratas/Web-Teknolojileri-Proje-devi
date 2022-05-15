6cef9d590761d63b50117b424072375203c5aeef
EmirhanKaratas <104207281+EmirhanKaratas@users.noreply.github.com>
2022-05-15 19:03:16 +0300

    Add files via upload
    Son hali

---
+++ login/login.php
@@ -0,0 +1,15 @@
+<?php
+$email=$_POST['email'];
+$sifre=$_POST['sifre'];
+
+
+if($email=="b211210073@sakarya.edu.tr"&&$sifre=="b211210073"){
+    
+    header("location:anasayfa.html");
+
+}
+else{
+    
+    header("location:http://localhost/login/login.html");
+    
+}
